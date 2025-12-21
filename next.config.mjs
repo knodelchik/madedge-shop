d6730fd68f02da53de8b5eab1cb59d1c7fdfc6d4
shramkovadym <vadymshramk@gmail.com>
2025-12-21 15:26:03 +0200

    redirects

--- next.config.mjs
+++ next.config.mjs
@@ -19,7 +19,22 @@ const nextConfig = {
     return [
       {
         source: '/c',
-        destination: '/contacts',
+        destination: '/contact',
+        permanent: true, // Це каже Гуглу, що сторінка переїхала назавжди
+      },
+      {
+        source: '/product-page/madedge-model-1',
+        destination: '/shop/madedge-model-1',
+        permanent: true, // Це каже Гуглу, що сторінка переїхала назавжди
+      },
+      {
+        source: '/blog',
+        destination: '/about',
+        permanent: true, // Це каже Гуглу, що сторінка переїхала назавжди
+      },
+      {
+        source: '/product-page/madedge-rotary-mechanism-model-1',
+        destination: '/shop/product-page/madedge-rotary-mechanism-model-1',
         permanent: true, // Це каже Гуглу, що сторінка переїхала назавжди
       },
     ];
